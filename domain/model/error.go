bef895dd111c4410d862f4ca6cbaeded5f8eb836
mahiro72 <mahiro.yoshie@gmail.com>
2023-10-28 18:29:55 +0900

    fix: typo

--- domain/model/error.go
+++ domain/model/error.go
@@ -3,9 +3,9 @@ package model
 import "errors"
 
 // user
-var ErrUserFieldsValidation = errors.New("failue user fields validation")
-var ErrUserIDValidation = errors.New("failue userID validation")
+var ErrUserFieldsValidation = errors.New("failure user fields validation")
+var ErrUserIDValidation = errors.New("failure userID validation")
 
 // post
-var ErrPostFieldsValidation = errors.New("failue post fields validation")
-var ErrPostIDValidation = errors.New("failue postID validation")
+var ErrPostFieldsValidation = errors.New("failure post fields validation")
+var ErrPostIDValidation = errors.New("failure postID validation")
